5a6e18d484da89a69fe32d68f9b90c3a8a9ce8d6
floatingmuseum <floatingmuseumyan@gmail.com>
2016-06-20 15:50:58 +0800

    add dagger2

---
+++ app/src/main/java/com/floatingmuseum/mocloud/mainmovie/MovieDetailActivity.java
@@ -0,0 +1,16 @@
+package com.floatingmuseum.mocloud.mainmovie;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Floatingmuseum on 2016/6/20.
+ */
+public class MovieDetailActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
