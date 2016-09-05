1869cfcb941465917e613a2c0e38e7f139088eea
floatingmuseum <floatingmuseumyan@gmail.com>
2016-09-05 09:35:46 +0800

    2016.9.5

--- app/src/main/java/com/floatingmuseum/mocloud/ui/mainmovie/trending/MovieTrendingPresenter.java
+++ app/src/main/java/com/floatingmuseum/mocloud/ui/mainmovie/trending/MovieTrendingPresenter.java
@@ -22,7 +22,7 @@ public class MovieTrendingPresenter implements MovieTrendingContract.Presenter, 
     private int pageNum = 1;
     protected Boolean shouldClean;
     private Repository repository;
-    
+
     @Inject
     MovieTrendingPresenter(@NonNull MovieTrendingContract.View trendingView,@NonNull Repository repository){
         this.repository = repository;
