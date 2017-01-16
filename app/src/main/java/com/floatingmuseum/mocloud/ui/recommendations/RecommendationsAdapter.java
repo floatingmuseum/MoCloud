3c1ca41760a56aaa80ef692d2c5fd3265f871309
floatingmuseum <floatingmuseumyan@gmail.com>
2017-01-16 17:04:45 +0800

    fixed RecommendationsAdapter

--- app/src/main/java/com/floatingmuseum/mocloud/ui/recommendations/RecommendationsAdapter.java
+++ app/src/main/java/com/floatingmuseum/mocloud/ui/recommendations/RecommendationsAdapter.java
@@ -1,13 +1,18 @@
 package com.floatingmuseum.mocloud.ui.recommendations;
 
+import android.support.v7.widget.CardView;
+
 import com.chad.library.adapter.base.BaseItemDraggableAdapter;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.floatingmuseum.mocloud.R;
 import com.floatingmuseum.mocloud.data.entity.Movie;
+import com.floatingmuseum.mocloud.data.entity.TmdbMovieImage;
 import com.floatingmuseum.mocloud.utils.ImageLoader;
 import com.floatingmuseum.mocloud.utils.NumberFormatUtil;
+import com.floatingmuseum.mocloud.utils.StringUtil;
 import com.floatingmuseum.mocloud.widgets.RatioImageView;
+import com.orhanobut.logger.Logger;
 
 import java.util.List;
 
@@ -15,7 +20,7 @@ import java.util.List;
  * Created by Floatingmuseum on 2017/1/16.
  */
 
-public class RecommendationsAdapter extends BaseItemDraggableAdapter<Movie,BaseViewHolder> {
+public class RecommendationsAdapter extends BaseItemDraggableAdapter<Movie, BaseViewHolder> {
 
     public RecommendationsAdapter(List<Movie> data) {
         super(R.layout.item_recommendations, data);
@@ -23,12 +28,23 @@ public class RecommendationsAdapter extends BaseItemDraggableAdapter<Movie,BaseV
 
     @Override
     protected void convert(BaseViewHolder baseViewHolder, Movie movie) {
-        baseViewHolder.setText(R.id.tv_movie_title,movie.getTitle())
-                .setText(R.id.tv_released,movie.getReleased())
-                .setText(R.id.tv_runtime,movie.getRuntime()+" mins")
-                .setText(R.id.tv_language,movie.getLanguage())
+        baseViewHolder.setText(R.id.tv_movie_title, movie.getTitle())
+                .setText(R.id.tv_released, movie.getReleased())
+                .setText(R.id.tv_runtime, movie.getRuntime() + " mins")
+                .setText(R.id.tv_language, movie.getLanguage())
                 .setText(R.id.tv_rating, NumberFormatUtil.doubleFormatToString(movie.getRating(), false, 2) + "/" + movie.getVotes() + "votes");
+        CardView cardView = baseViewHolder.getView(R.id.cardview);
+//        cardView.setForeground(android.R.attr.selectableItemBackground);
         RatioImageView ivPoster = baseViewHolder.getView(R.id.iv_poster);
-        ImageLoader.load(mContext,movie.getImage().getCacheFile(),ivPoster,R.drawable.default_movie_poster);
+        TmdbMovieImage image = movie.getImage();
+        if (image.isHasCache()) {
+            ImageLoader.load(mContext, image.getCacheFile(), ivPoster, R.drawable.default_movie_poster);
+        } else {
+            if (image.isHasPoster()) {
+                ImageLoader.load(mContext, StringUtil.buildPosterUrl(image.getPosters().get(0).getFile_path()), ivPoster, R.drawable.default_movie_poster);
+            } else {
+                ImageLoader.loadDefault(mContext, ivPoster);
+            }
+        }
     }
 }
